90422cfcee7bcd5344a7123a6392cd4b14f4cea0
Francesco Porcari <francesco.porcari@softwell.it>
2020-01-13 20:35:40 +0100

    fix

--- packages/lrn/model/topic_alias.py
+++ packages/lrn/model/topic_alias.py
@@ -5,6 +5,8 @@ class Table(object):
         tbl =  pkg.table('topic_alias',pkey='id',name_long='!![en]Topic alias',
                       name_plural='!![en]Topic alias')
         self.sysFields(tbl)
+        tbl.column('topic_id',size='22', group='_', name_long='Topic'
+                    ).relation('topic.id', relation_name='aliases', mode='foreignkey', onDelete='raise')
         tbl.column('question_id',size='22',group='_',name_long='!![en]Question').relation('question.id', 
                                         mode='foreignkey', onDelete='cascade',
                                         relation_name='questions_alias')
