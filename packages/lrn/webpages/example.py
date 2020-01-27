f085ef6208a0c3adf7a61640f9d00ee39b66317f
Saverio Porcari <>
2020-01-27 20:05:57 +0100

    no message

--- packages/lrn/webpages/example.py
+++ packages/lrn/webpages/example.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
-            
+from gnr.core.gnrbag import Bag
+
 class GnrCustomWebPage(object):
     py_requires = 'miocomponent:GestoreAnagrafiche'
 
     def main(self,root,color='gray',**kwargs):
-        root.div('Hello',color=color)
+        #metto dei dati di partenza
+        root.data('main.dati_anagrafici', Bag(nome='Anselmo', cognome='Rossi'))
+
+        pane = root.contentPane(datapath='main.dati_anagrafici')
+        fb = pane.formbuilder(cols=2, nodeId='miaform')
+        fb.textbox('^.nome', lbl='Nome')
+        fb.textbox('^.cognome', lbl='Cognome')
+        fb.button('Premi', action="alert('Ciao '+ nome +' '+cognome)",
+                    nome='=#miaform.nome', 
+                    cognome='=#miaform.cognome')
     
     
 
